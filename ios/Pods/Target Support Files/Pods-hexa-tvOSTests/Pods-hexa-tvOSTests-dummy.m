b335432b7ab7d503b389e36b701d4439f179356a
Bryce Bjork <brycedbjork@gmail.com>
2017-11-15 00:17:17 -0500

    getting firebase to work

---
+++ ios/Pods/Target Support Files/Pods-hexa-tvOSTests/Pods-hexa-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_hexa_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_hexa_tvOSTests
+@end
--- src/firestore/Person.js
+++ src/firestore/Person.js
@@ -1,11 +1,19 @@
-import { NewPersonalWallet } from 'src/firestore/Wallet';
 
+import firebase from 'react-native-firebase';
+let firestore = firebase.firestore();
 
 function NewPerson(first_name, last_name, email, phone_number, facebook_id, picture_url, address, city, state,
                    zip_code, country, coinbase_id=null) {
 
     // create firestore person entity
+    var newPersonRef = firestore.collection("people").add({
+        first_name: "Lukas",
+        last_name: "Burger"
+    });
+
 
     // create personal starter wallet with NewPersonalWallet
 
 }
+
+export { NewPerson };
