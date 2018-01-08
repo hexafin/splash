7965e33bf82dca524ae13e861b3b3d2e4e9135a0
Bryce Bjork <brycedbjork@gmail.com>
2018-01-08 01:36:06 -0500

    bitcoin and ethereum images

--- src/lib/styles.js
+++ src/lib/styles.js
@@ -4,8 +4,8 @@ import {ifIphoneX} from "react-native-iphone-x-helper"
 
 export const defaults = {
 	shadowOffset: {
-			width: 0,
-			height: 10,
+		width: 0,
+		height: 10,
 	},
 	shadowOpacity: 0.1,
 	shadowRadius: 24,
