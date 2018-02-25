e8d04629260907e0c46fe13d87489d3845f8c78f
Bryce Bjork <brycedbjork@gmail.com>
2018-02-25 00:27:19 -0500

    custom router

---
+++ src/components/ChooseSplashtag/index.js
@@ -0,0 +1,19 @@
+import ChooseSplashtag from "./ChooseSplashtag"
+import {connect} from "react-redux"
+import {bindActionCreators} from "redux"
+import {ClaimUsername, InviteFriends} from "../../actions/waitlist"
+
+const mapStateToProps = (state) => {
+    return {
+      waitlisted: state.general.waitlisted,
+      username: state.general.person.username
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        ClaimUsername, InviteFriends
+    }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChooseSplashtag)
