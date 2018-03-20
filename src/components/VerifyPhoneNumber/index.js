15a4f371d1a6bda5ba0f7ed8f4232a4fd0a7027e
Bryce Bjork <brycedbjork@gmail.com>
2018-03-20 13:56:54 -0400

    loading when confirming sms

--- src/components/VerifyPhoneNumber/index.js
+++ src/components/VerifyPhoneNumber/index.js
@@ -1,23 +1,28 @@
-import VerifyPhoneNumber from "./VerifyPhoneNumber"
-import {connect} from "react-redux"
-import {bindActionCreators} from "redux"
-import {SmsConfirm, SmsAuthenticate} from "../../actions/waitlist"
+import VerifyPhoneNumber from "./VerifyPhoneNumber";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { SmsConfirm, SmsAuthenticate } from "../../actions/waitlist";
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
     return {
-      waitlisted: state.general.waitlisted,
-      username: state.waitlist.username,
-      phoneNumber: state.waitlist.phoneNumber,
-      countryName: state.waitlist.countryName,
-      confirmError: state.waitlist.errorSmsConfirming,
-      claimError: state.waitlist.errorClaimingUsername,
-    }
-}
+        waitlisted: state.general.waitlisted,
+        username: state.waitlist.username,
+        phoneNumber: state.waitlist.phoneNumber,
+        countryName: state.waitlist.countryName,
+        confirmError: state.waitlist.errorSmsConfirming,
+        claimError: state.waitlist.errorClaimingUsername,
+        isSmsConfirming: state.waitlist.isSmsConfirming
+    };
+};
 
-const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators({
-        SmsConfirm, SmsAuthenticate
-    }, dispatch)
-}
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators(
+        {
+            SmsConfirm,
+            SmsAuthenticate
+        },
+        dispatch
+    );
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(VerifyPhoneNumber)
+export default connect(mapStateToProps, mapDispatchToProps)(VerifyPhoneNumber);
