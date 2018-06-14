e6b59fcd6aa52b79954d6da6fc4ae1b69a454e66
Lukas Burger <lukas.burger@yale.edu>
2018-06-14 17:08:30 +0200

    working account deletion

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -5,21 +5,24 @@ import {resetUser} from "../../redux/user/actions"
 import {resetTransactions} from "../../redux/transactions/actions"
 import {resetOnboarding} from "../../redux/onboarding/actions"
 import NavigatorService from "../../redux/navigator"
+import api from '../../api'
 
 
 const mapStateToProps = state => {
 	return {
-		splashtag: state.user.entity.splashtag
+		splashtag: state.user.entity.splashtag,
+		userId: state.user.id,
 	}
 }
 
 const mapDispatchToProps = dispatch => {
 	return {
-		logout: () => {
+		logout: (userId) => {
 			dispatch(resetUser())
 			dispatch(resetTransactions())
 			dispatch(resetOnboarding())
 			NavigatorService.navigate("Landing")
+			api.DeleteAccount(userId)
 		}
 	}
 }
